b01bd5ea93ad53de7cbee8bc746ad651201febee
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2026-05-21 20:02:24 +0200

    Start working on camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float mouseSensivity = 100f;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
