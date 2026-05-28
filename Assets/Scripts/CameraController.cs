41838dd9b6deef58d38256671ba4d6dc5722177a
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2026-05-28 19:08:25 +0200

    Main Camera Rotates

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,35 @@
+using System;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] float mouseSensivity = 100f;
+    Transform playerBody;
+    float xRotation = 0f;
 
 
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        playerBody = transform.parent;
     }
 
 
     void Update()
     {
-        
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Math.Clamp(xRotation, -90f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+
     }
 }
