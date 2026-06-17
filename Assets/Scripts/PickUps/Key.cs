6bd09a921a452d1442f92f389ee4ef93fb66ec59
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2026-06-17 12:28:54 +0200

    Keys Added

---
+++ Assets/Scripts/PickUps/Key.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+public enum KeyColor
+{
+    Red,
+    Green,
+    Gold
+}
+public class Key : PickUp
+{
+    public KeyColor color;
+    public override void Picked()
+    {
+        GameManager.gameManager.AddKey(color);
+        Destroy(this.gameObject);
+    }
+    void Update()
+    {
+        Rotation();
+    }
+}
