0149eb2f55e690468adc62eb81a0c9f2d0917098
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2026-06-25 20:02:11 +0200

    Portal done

--- Assets/Scripts/PortalTeleport.cs
+++ Assets/Scripts/PortalTeleport.cs
@@ -25,4 +25,28 @@ public class PortalTeleport : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        Teleportation();
+    }
+
+    void Teleportation()
+    {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+            if (dotProduct < 0f)
+            {
+                float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) *
+                portalToPlayer;
+                player.position = receiver.position + positionOffset;
+                playerIsOverlapping = false;
+            }
+        }
+    }
+
 }
