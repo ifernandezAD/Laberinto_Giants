3fc46546bfb08935b6ed3ec6494b341afffcda2e
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2026-07-27 15:34:07 +0200

    Auto generate map script added

---
+++ Assets/Scripts/ColorToPrefab.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+[System.Serializable]
+public class ColorToPrefab
+{
+    public Color color;
+    public GameObject prefab;
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D map;
+    public ColorToPrefab[] colorMappings;
+    public float offset = 5f;
+    public Material material01;
+    public Material material02;
+    public void GenerateLabirynth()
+    {
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int z = 0; z < map.height; z++)
+            {
+                GenerateTile(x, z);
+            }
+        }
+        ColorTheChildren();
+    }
+    void GenerateTile(int x, int z)
+    {
+        Color pixelColor = map.GetPixel(x, z);
+        bool pyklo = false;
+        foreach (ColorToPrefab colorMapping in colorMappings)
+        {
+            if (colorMapping.color.Equals(pixelColor))
+            {
+                Vector3 position = new Vector3(x, 0, z) * offset;
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+    public void ColorTheChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.tag == "Wall")
+            {
+                if (Random.Range(1, 100) % 3 == 0)
+                {
+                    child.gameObject.GetComponent<Renderer>().material = material02;
+                }
+                else
+                {
+                    child.gameObject.GetComponent<Renderer>().material = material01;
+                }
+            }
+            if (child.childCount > 0)
+            {
+                foreach (Transform grandchild in child.transform)
+                {
+                    if (grandchild.tag == "Wall")
+                    {
+                        if (Random.Range(1, 100) % 3 == 0)
+                        {
+                            grandchild.gameObject.GetComponent<Renderer>().material =
+                            material02;
+                        }
+                        else
+                        {
+                            grandchild.gameObject.GetComponent<Renderer>().material =
+                            material01;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
