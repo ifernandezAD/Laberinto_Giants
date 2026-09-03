5c5bec4091d8f4cc29a62f9ca5d4e8fbb06fa11c
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2026-09-03 20:05:44 +0200

    Laberinth added

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D map;
+    public ColorToPrefab[] colorMappings;
+    public float offset = 5f;
+
+    public Material material01;
+    public Material material02;
+
+    void GenerateTile(int x, int z)
+    {
+        Color pixelColor = map.GetPixel(x, z);
+        foreach (ColorToPrefab colorMapping in colorMappings)
+        {
+            if (colorMapping.color.Equals(pixelColor))
+            {
+                Vector3 position = new Vector3(x, 0, z) * offset;
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+
+    public void ColorTheChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.tag == "Wall")
+            {
+                if (Random.Range(1, 100) % 3 == 0)
+                {
+                    child.gameObject.GetComponent<Renderer>().material = material02;
+                }
+                else
+                {
+                    child.gameObject.GetComponent<Renderer>().material = material01;
+                }
+            }
+            if (child.childCount > 0)
+            {
+                foreach (Transform grandchild in child.transform)
+                {
+                    if (grandchild.tag == "Wall")
+                    {
+                        if (Random.Range(1, 100) % 3 == 0)
+                        {
+                            grandchild.gameObject.GetComponent<Renderer>().material =
+                            material02;
+                        }
+                        else
+                        {
+                            grandchild.gameObject.GetComponent<Renderer>().material =
+                            material01;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void GenerateLabirynth()
+    {
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int z = 0; z < map.height; z++)
+            {
+                GenerateTile(x, z);
+            }
+        }
+        ColorTheChildren();
+    }
+
+}
