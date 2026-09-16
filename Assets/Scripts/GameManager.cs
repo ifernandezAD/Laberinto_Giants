556cd598b7c68b17f8f284062bae7fb93b744ffc
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2026-09-16 15:51:48 +0200

    Game Finished

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Threading;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -24,10 +25,16 @@ public class GameManager : MonoBehaviour
     public AudioClip winClip;
     public AudioClip loseClip;
 
-    [Header("Debug")]
-    public TextMeshProUGUI debugCounterText;
-    public GameObject debugCounter;
-    public GameObject debugPausePopup;
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI goldKeyText;
+    public TextMeshProUGUI redKeyText;
+    public TextMeshProUGUI greenKeyText;
+    public TextMeshProUGUI crystalText;
+    public Image snowFlake;
+    public GameObject infoPanel;
+    public TextMeshProUGUI pauseEnd;
+    public TextMeshProUGUI reloadInfo;
+    public TextMeshProUGUI useInfo;
 
 
     void Start()
@@ -42,14 +49,19 @@ public class GameManager : MonoBehaviour
             timeToEnd = 100;
         }
 
+        snowFlake.enabled = false;
+        timeText.text = timeToEnd.ToString();
+        infoPanel.SetActive(false);
+        pauseEnd.text = "Pause";
+        reloadInfo.text = "";
+
         audioSource = GetComponent<AudioSource>();
         InvokeRepeating("Stopper", 2, 1);
     }
 
     private void Update()
     {
-        PauseCheck();
-        DebugPickUps(); // Borrar luego
+        PauseCheck(); 
     }
 
     private void PauseCheck()
@@ -70,14 +82,14 @@ public class GameManager : MonoBehaviour
     void Stopper()
     {
         timeToEnd--;
+        timeText.text = timeToEnd.ToString();
+        snowFlake.enabled = false;
 
         if (timeToEnd <= 0)
         {
             timeToEnd = 0;
             endGame = true;
         }
-
-        DebugUpdateTimerUI();
 
         if (endGame)
         {
@@ -88,32 +100,32 @@ public class GameManager : MonoBehaviour
     public void PauseGame()
     {
         PlayClip(pauseClip);
-        Debug.Log("Game paused");
+        infoPanel.SetActive(true);
         Time.timeScale = 0;
         gamePaused = true;
-        debugPausePopup.SetActive(true);
-        debugCounter.SetActive(false);
     }
 
     public void ResumeGame()
     {
-        Debug.Log("Game resumed");
+        infoPanel.SetActive(false);
         Time.timeScale = 1;
         gamePaused = false;
-        debugPausePopup.SetActive(false);
-        debugCounter.SetActive(true);
     }
 
     public void EndGame()
     {
         CancelInvoke("Stopper");
+        infoPanel.SetActive(true);
 
         if (win)
         {
-            Debug.Log("You win!!Reload?");
-        }else
+            pauseEnd.text = "You Win!!!";
+            reloadInfo.text = "Reload? Y/N";
+        }
+        else
         {
-            Debug.Log("You Lose!!Reload?");
+            pauseEnd.text = "You Lose!!!";
+            reloadInfo.text = "Reload? Y/N";
         }
     }
 
@@ -128,16 +140,19 @@ public class GameManager : MonoBehaviour
     public void AddPoints(int point)
     {
         points += point;
+        crystalText.text = points.ToString();
     }
 
     public void AddTime(int addTime)
     {
         timeToEnd += addTime;
+        timeText.text = timeToEnd.ToString();
     }
 
     public void FreezTime(int freez)
     {
         CancelInvoke("Stopper");
+        snowFlake.enabled = true;
         InvokeRepeating("Stopper", freez, 1);
     }
 
@@ -147,38 +162,21 @@ public class GameManager : MonoBehaviour
         if (color == KeyColor.Gold)
         {
             goldKey++;
+            goldKeyText.text = goldKey.ToString();
         }
         else if (color == KeyColor.Green)
         {
             greenKey++;
+            greenKeyText.text = greenKey.ToString();
         }
         else if (color == KeyColor.Red)
         {
             redKey++;
+            redKeyText.text = redKey.ToString();
         }
     }
 
     #endregion
 
-    #region Debug
-
-    void DebugUpdateTimerUI()
-    {
-        int minutes = timeToEnd / 60;
-        int seconds = timeToEnd % 60;
-
-        debugCounterText.text = $"{minutes:00}:{seconds:00}";
-    }
-
-    void DebugPickUps()
-    {
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            Debug.Log("Actual Time: " + timeToEnd);
-            Debug.Log("Points: " + points);
-            Debug.Log($"Red Key:{redKey}, Green Key:{greenKey}, Gold Key:{goldKey}");
-        }
-    }
-
-    #endregion
+    
 }
